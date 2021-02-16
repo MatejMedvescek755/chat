e34829204591f19520b8255df34de45de3297a10
matej medvescek <matej.medvescek@tscng.org>
2021-02-16 18:57:22 +0100

    the msg should display now :?

--- chat/src/App.js
+++ chat/src/App.js
@@ -19,7 +19,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const firestore = firebase.firestore
+const firestore = firebase.firestore();
 function App() {
 
   const [user] = useAuthState(auth);
@@ -51,17 +51,18 @@ function SignIn(){
 function Chat(){
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
-  const [messages] = useCollectionData(query);
+  const [messages] = useCollectionData(query, {idField:'id'});
   return(
 
     <div>
-    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+     {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      {console.log(messages)}
     </div>
   )
   
 }
 function ChatMessage(props) {
-  const {text, uid} = props.message;
+  const { text } = props.message;
   return <p>{text}</p>
 }
 export default App;
