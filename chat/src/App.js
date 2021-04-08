e5a27737c8e5c490aa4b09abf24dc848b23517e1
matej medvescek <matej.medvescek@tscng.org>
2021-04-08 18:49:39 +0200

    added env

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
@@ -7,8 +6,15 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+console.log(process.env.REACT_APP_API_KEY)
 const firebaseConfig = {
   //CONFIG
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -59,7 +65,7 @@ function Chat(){
   return(
 
     <div>
-     {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
     </div>
   )
   
